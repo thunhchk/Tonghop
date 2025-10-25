723de9e717ddecc82b9085d4afb672671bfbdb33
thunhchk <thinhff339@gmail.com>
2025-10-25 15:05:48 +0700

    Add files via upload

---
+++ tonghopvip.lua
@@ -0,0 +1,76 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "thinh Roblox tổng hợp",
+         Animation = "thịnh Roblox hack"
+         },
+        Key = {
+        KeySystem = true,
+        Title = "bản premium nên có key nhé",
+        Description = "",
+        KeyLink = "",
+        Keys = {"thinhyeujack"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=86749587185394",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab0o = MakeTab({Name = "mm2"})
+     local Tab1o = MakeTab({Name = "gow a gader"})
+     local Tab2o = MakeTab({Name = "99 đêm"})
+     local Tab3o = MakeTab({Name = "steal a brainot"})
+     local Tab4o = MakeTab({Name = "plant vs brainot"})
+     local Tab5o = MakeTab({Name = "srcipt linh tinh"})
+     local Tab6o = MakeTab({Name = "blox fruit"})
+     
+------- BUTTON
+    
+    AddButton(Tab6o, {
+     Name = "thinh limeted",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/thunhchk/thinhlimeted/refs/heads/main/tthinhlimeted.lua"))()
+  end
+  })
+  
+  AddButton(Tab5o, {
+     Name = "bất tử tất cả game",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/Roblox-Scripts/refs/heads/main/God%20Mode%20Script%20Universal"))()
+  end
+  })
+  
+  AddButton(Tab5o, {
+     Name = "bất tử 99 đêm",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/ProBaconHub/DATABASE/refs/heads/main/99%20Nights%20in%20the%20Forest/Infinite%20Health.lua"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "maru hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/thunhchk/Maruvietsub/refs/heads/main/maruhubthinh.txt"))()
+  end
+  })
+  
+  AddButton(Tab0o, {
+     Name = "ko biết tên",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/NexusScripts212/Roblox/refs/heads/main/Mm2",true))()
+  end
+  })
