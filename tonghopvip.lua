b2931875d45928465a2c5705d8f8290f2fcddf03
thunhchk <thinhff339@gmail.com>
2025-10-25 15:48:42 +0700

    Add files via upload

--- tonghopvip.lua
+++ tonghopvip.lua
@@ -37,6 +37,8 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
      local Tab4o = MakeTab({Name = "plant vs brainot"})
      local Tab5o = MakeTab({Name = "srcipt linh tinh"})
      local Tab6o = MakeTab({Name = "blox fruit"})
+     local Tab7o = MakeTab({Name = "kaitun blox fruit"})
+     local Tab8o = MakeTab({Name = "script auto Bounty"})
      
 ------- BUTTON
     
@@ -142,5 +144,437 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
      Name = "99 đêm thịnh",
     Callback = function()
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/thunhchk/99nigth/refs/heads/main/99nightthinh.lua"))()
+  end
+  })
+  
+  AddButton(Tab5o, {
+     Name = "bất tử",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/Roblox-Scripts/refs/heads/main/God%20Mode%20Script%20Universal"))()
+  end
+  })
+  
+  AddButton(Tab7o, {
+     Name = "kaitun thịnh",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/thunhchk/Kaitunbf/refs/heads/main/thinhkaitunbf.txt"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "doremon v2",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/KiddoHiru/BloxFruits/main/MasterHub.lua"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "zen hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://zuwz.me/Ls-Zee-Hub"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "w azure",
+    Callback = function()
+	  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/85e904ae1ff30824c1aa007fc7324f8f.lua"))()
+  end
+  })
+  
+  AddButton(Tab5o, {
+     Name = "hoho hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "blue x hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/Main.lua"))()
+  end
+  })
+  
+  AddButton(Tab5o, {
+     Name = "blue x hub nhặt trái",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/FindFruits.lua"))()
+  end
+  })
+  
+  AddButton(Tab5o, {
+     Name = "teddy hop sever",
+    Callback = function()
+	  repeat task.wait() until game:IsLoaded() and game:GetService("Players") and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/TEDDYHUB-FREEMIUM"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "quantum",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "thunder",
+    Callback = function()
+	  loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua'))()
+  end
+  })
+  
+  AddButton(Tab2o, {
+     Name = "quantum",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
+  end
+  })
+  
+  AddButton(Tab8o, {
+     Name = "Crystal Hub",
+    Callback = function()
+	  getgenv().team = "Pirates" -- Pirates // Marines
+
+Setup = {
+    ["Auto Team"] = {
+        ["Enable Lock Bounty"] = false,
+        ["Lock Bounty"] = {0, 300000000},
+    },
+    ["Panic % Health"] = {35,50},
+    ["Chat Kill"] = {
+        ["Enabled"] = false,
+        ["Message"] = {"No Skill? Use Crystal Hup Now", "i have a good gaming chair"}
+    },
+    ["Lock Cam"] = false,
+    ["Enable Cam Farm"] = false,
+    ["Postion"] = {
+        ["Random Y Tween"] = false,
+        ["Custom Y Run"] = {
+            ["Enabled"] = false,
+            ["Y Run"] = 100
+        },
+    },
+    ["Click Delay"] = 0.175
+}
+
+Hunter = {
+    ["Ignore"] = {
+        ["Fruit"] = {["Enabled"] = true,
+            ["List"] = {
+                "Portal-Portal",
+            }
+        },
+        ["V4"] = false
+    },
+    ["Use Move Predict"] = true,
+    ["Hit and Run"] = true,
+    ["Gun Method"] = false,
+    ["Spam All Skill On V4"] = {
+        ["Enabled"] = true,
+        ["Weapons"] = {"Melee", "Blox Fruit"}
+    },
+}
+
+Booster = {
+    ["White Screen"] = false,
+    ["HightLight Mode"] = true,
+    ["Hide Map"] = false,
+}
+
+Skills = {
+    --// explain ["Enabled"] = {true, 1.5}, [ Enable Weapon , Hold Time Weapon ]
+    --// explain ["Z"] = {true, 0, 0, 1},  [ Enable Skill , Hold Time Skill , Time To Next Skill, Priority ]
+    ["Use"] = {"Melee", "Blox Fruit"},
+    ["Melee"] = {
+        ["Enabled"] = {true, 0.8},
+        ["Switch Next Weapon If Cooldown"] = true,
+        ["Z"] = {true, ["NoPredict"] = false, 0.2, 0, 2}, -- // ["NoPredict"] For Dragon Tailon, Disable it
+        ["X"] = {true, 0.14, 0, 3}, 
+        ["C"] = {true, 0.14, 0, 1},
+        ["V"] = {false, 0, 4} 
+    },
+    ["Blox Fruit"] = {
+        ["Enabled"] = {true, 2.2},
+        ["Switch Next Weapon If Cooldown"] = true,
+        ["Z"] = {true, 1.5, 0, 1}, 
+        ["X"] = {true, 0.3, 0, 2}, 
+        ["C"] = {true, 0.1, 0, 3}, 
+        ["V"] = {true, 0.2, 0, 4}, 
+        ["F"] = {true, 0, 0, 5}
+    },
+    ["Sword"] = {
+        ["Enabled"] = {false, 1},
+        ["Switch Next Weapon If Cooldown"] = true,
+        ["Z"] = {true, 0.1, 0, 2}, 
+        ["X"] = {true, 0.2, 0, 3}, 
+    },
+    ["Gun"] = {
+        ["Enabled"] = {false, 0.4},
+        ["Switch Next Weapon If Cooldown"] = false,
+        ["Z"] = {true, 0, 0.05, 1}, 
+        ["X"] = {true, 0, 0.05, 2}, 
+    }
+}
+
+Counter = {
+    Theme = {
+        ["Background"] = "18675204938",
+        ["Character"] = "18388637422", 
+        ["Icon"] = "18675236797",
+        ["Button"] = "18675335801",
+        ["Color"] = {
+            ["Text"] = Color3.fromRGB(221, 86, 92),
+            ["Stroke"] = Color3.fromRGB(104, 47, 46)
+        }
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shinichi-dz/phucshinsayhi/refs/heads/main/AutoBounty.lua"))()
+  end
+  })
+  
+  AddButton(Tab8o, {
+     Name = "VisionX",
+    Callback = function()
+	  _G.config = {
+    team = "Pirates", --? Pirates Marines
+    servertohop = "Singapore",
+    timetoskip = 80,
+    timetohop = 600,
+    autouselowgraphic = true,
+    autoQ = false,
+    random = true, --! random = false if u want to use custom
+    autoken = true,
+    enablev4 = true,
+    enablev3 = true,
+    blackscreen = false,
+    ignorefruits = {"Portal-Portal"--[[, "Buddha-Buddha", "Dragon-Dragon", "Kitsune-Kitsune", "Leopard-Leopard"]]},
+    safezone = {
+        HighestHealth = 65, -- % health
+        LowestHealth = 35, -- % health
+    },
+    methodclicks = {
+        LowerHealthToM1 = 4000,
+        Delay = 0.25,
+        Melee = true,
+        Sword = false,
+        Gun = false,
+    },
+    custom = {
+        Melee = {
+            Enable = true,
+            Skills = {
+                Z = {
+                    Enable = true,
+                    Number = 3,
+                    HoldTime = 0.15151515,
+                },
+                X = {
+                    Enable = true,
+                    Number = 5,
+                    HoldTime = 0.15151515,
+                },
+                C = {
+                    Enable = true,
+                    Number = 4,
+                    HoldTime = 0.15151515,
+                },
+            },
+        },
+        Sword = {
+            Enable = true,
+            Skills = {
+                Z = {
+                    Enable = true,
+                    Number = 1,
+                    HoldTime = 0.15151515,
+                },
+                X = {
+                    Enable = true,
+                    Number = 2,
+                    HoldTime = 0.15151515,
+                },
+            },
+        },
+        ['Blox Fruit'] = {
+            Enable = false,
+            Skills = {
+                Z = {
+                    Enable = true,
+                    Number = 4,
+                    0.15151515,
+                },
+                X = {
+                    Enable = true,
+                    Number = 1,
+                    HoldTime = 0.15151515,
+                },
+                C = {
+                    Enable = true,
+                    Number = 4.5,
+                    HoldTime = 0.15151515,
+                },
+                V = {
+                    Enable = true,
+                    Number = 7,
+                    HoldTime = 0.15151515,
+                },
+                F = {
+                    Enable = true,
+                    Number = 8,
+                    HoldTime = 0.15151515,
+                },
+            },
+        },
+        Gun = {
+            Enable = true,
+            Skills = {
+                Z = {
+                    Enable = true,
+                    Number = 5,
+                    HoldTime = 0.15151515,
+                },
+                X = {
+                    Enable = true,
+                    Number = 1,
+                    HoldTime = 0.15151515,
+                },
+            },
+        },
+    },
+    webhook = {
+        Enabled = true,
+        Url = "",
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RedGamer12/VisionX/refs/heads/main/bounty-obfuscated.lua"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "tboy Roblox",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/TBoyRoblox727/TBoyRobloxYTB/refs/heads/main/TBeoMainBF.lua
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "xeter v3",
+    Callback = function()
+	  getgenv().Version = "V3"
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TlDinhKhoi/Xeter/refs/heads/main/Main.lua"))()
+  end
+  })
+  
+  AddButton(Tab7o, {
+     Name = "Yiner Hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-Yiner/Yiner-Hub/refs/heads/main/KaitunOfTheSkies.lua"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "ngọc bổng",
+    Callback = function()
+	  getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ngocbonggaming/script/refs/heads/main/NgocBongVn.lua"))()
+  end
+  })
+  
+  AddButton(Tab2o, {
+     Name = "auto kc 99 đêm",
+    Callback = function()
+	  getgenv().WebhookURL = ""
+getgenv().AutoFarm = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/Farm%20Diamond%20v2.lua"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "hiru hub",
+    Callback = function()
+	  repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaStupid/ExecuteGames/main/QuestGames.lua"))()
+  end
+  })
+  
+  AddButton(Tab4o, {
+     Name = "saki hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://saki-hub.vercel.app/pvb.lua"))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "OMG hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/MainLoader.lua"))()
+  end
+  })
+  
+  AddButton(Tab7o, {
+     Name = "zero hub kaitun",
+    Callback = function()
+	  getgenv().Team = "Pirates"
+getgenv().Config = {
+    ["Farm"] = {
+        ["Skip Level"] = true,
+        ["Raids"] = { 
+            ["Lock Fragment"] = 15000,
+            ["Awakening Fruit"] = false
+        },
+        ["Lock Race"] = {},
+        ["Evo Race"] = true
+    },
+    ["Quest"] = {
+        ["Citizen Quest"] = true,
+        ["RGP Color"] = true,
+        ["Pull Lever"] = false
+    },
+    ["Item"] = {
+        ["Get Pole"] = false,
+        ["Get Saber"] = true,
+        ["Get Soul Cane"] = true,
+        ["Get Bisento"] = true,
+        ["Get Rengoku"] = true,
+        ["Get Midnight Blade"] = true,
+        ["Skull Guitar"] = true,
+        ["Cursed Dual Katana"] = true,
+        ["Get Kabucha"] = true,
+        ["Get Mirror Fractal when have God's Chalice"] = true,
+        ["Collect Berries"] = false
+    },
+    ["Settings"] = {
+        ["FPS Boost"] = true,
+        ["Black Screen"] = false,
+        ["Method"] = "Half", -- Mastery Sword
+        ["Buy Zoro Sw"] = false,
+        ["Bring Range"] = 350,
+        ["Hop if any player is nearby"] = true
+    },
+    ["Fruit"] = {
+        ["Select Fruit"] = "", -- Dough-Dough
+        ["Snipe Fruit"] = false
+    }
+} 
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ca16cb42816b395e079fa7a114352ba9.lua"))()
+  end
+  })
+  
+  AddButton(Tab6o, {
+     Name = "zis roblox",
+    Callback = function()
+	  getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Zis/refs/heads/main/ZisBVn"))()
+  end
+  })
+  
+  AddButton(Tab5o, {
+     Name = "đạt thg",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/DatThg/refs/heads/main/DatThgV2"))()
   end
   })
